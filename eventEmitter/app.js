f7ca04c19687a898c45098cbc44047db28698187
1712878 <1712878@student.hcmus.edu.vn>
2020-02-08 20:50:53 +0700

    ~

--- eventEmitter/app.js
+++ eventEmitter/app.js
@@ -5,17 +5,10 @@ var emitter = new Emitter();
 emitter.on("bad", function(){
     console.log("Một môn nào đó bị điểm kém !!");
 });
-console.log(emitter);
-
-emitter.on("good", function(){
-    console.log("Đã có điểm kém, cần phát thông báo tới phụ huynh!");
-});
-console.log(emitter);
 
 emitter.on("bad", function(){
     console.log("Đã có điểm kém, cần phát thông báo tới phụ huynh!");
 });
-console.log(emitter);
 
 // có 1 bảng điểm
 var scores = [10, 4];
